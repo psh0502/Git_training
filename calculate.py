e26757433007738ce28895db11fc8a70da31579e
psh0502 <pngasc@gmail.com>
2021-11-16 19:06:27 +0900

    one sentence cal program

--- calculate.py
+++ calculate.py
@@ -1,1 +1,18 @@
 #calculator program
+
+print("two number calculator")
+print("You can input formula")
+print()
+print()
+
+formula = str(input("input formula : "))
+divided = formula.split()
+
+if(str(divided[1]) == "+"):
+	print("Result = " + str(int(divided[0]) + int(divided[2])))
+elif(str(divided[1]) == "-"):
+	print("Result = " + str(int(divided[0]) - int(divided[2])))
+elif(str(divided[1]) == "*"):
+	print("Result = " + str(float(divided[0]) * float(divided[2])))
+elif(str(divided[1]) == "/"):
+	print("Result = " + str(float(divided[0] / float(divided[2])))
